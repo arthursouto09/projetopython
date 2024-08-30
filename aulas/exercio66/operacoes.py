fbd0f303899efa85a3ef2fbb1d9f19a6634fb24b
arthursouto09 <arthur090810@gmail.com>
2024-08-30 17:45:32 -0300

    projetopython

---
+++ aulas/exercio66/operacoes.py
@@ -0,0 +1,21 @@
+nomes =[]
+
+def menu():
+    opcoes = ['Cadastrar nome', 'Atualizar nome', 'Excluir nome', 'Listar nomes']
+
+    for indice ,opcao in enumerate(opcoes):
+        print(f'{indice + 1} - {opcao}')
+
+
+def listar_nomes():
+     for indice ,nome in enumerate(nomes):
+                print(f'{indice} - {nome}')
+
+def cadastrar_nome(nome):
+    nomes.append(nome)
+
+def atualiza_nome(nome, novo_nome):
+    nome[nomes.index(nome)] = novo_nome
+
+def excluir_nome(nome):
+    nomes.remove(nome)
